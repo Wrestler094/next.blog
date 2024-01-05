d6df45d5d95bba6f7a4e45db5333db065263116d
Mikhail Kovalev <kovalev094@gmail.com>
2024-01-05 16:32:04 +0300

    Finished task 1 part 4

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image';
 import {Metadata} from "next";
 import LikeBlock from "@/components/LikeBlock/LikeBlock";
+import Tag from "@/components/Tag/Tag";
+import Like from "@/components/Like/Like";
+import {capitalizeFirstLetter} from "@/helpers/capitalizeFirstLetter";
+import {getRandomNumber} from "@/helpers/getRandomNumber";
+import {getFakeCategory, getFakePublicationDate, getFakeReadEstimation} from "@/helpers/getFakeData";
 import {getPost, getPosts} from "@/api/jsonplaceholder";
 
-import {capitalizeFirstLetter} from "@/helpers/capitalizeFirstLetter";
+import postCard from '../../../public/images/post-image.png';
+
+import styles from './page.module.scss';
 
 type Params = {
     params: {
@@ -28,14 +36,23 @@ export async function generateStaticParams() {
     }));
 }
 
-export default async function PostsPage({params}: Params) {
+export default async function PostPage({params}: Params) {
     const post = await getPost(params.id);
 
     return (
-        <main>
-            <p>
-                {JSON.stringify(post)}
-            </p>
+        <main className={styles.wrapper}>
+            <h2 className={styles.title}>{capitalizeFirstLetter(post.title)}</h2>
+            <div className={styles['post-info']}>
+                <Tag>{getFakeCategory()}</Tag> · <Tag isLight={true}>{getFakePublicationDate()}</Tag> · <Tag
+                isLight={true}>{getFakeReadEstimation()}</Tag> · <Like numberOfLikes={getRandomNumber(0, 8)}/>
+            </div>
+            <Image
+                src={postCard}
+                alt="Post image"
+                className={styles.image}
+                priority={false}
+            />
+            <p dangerouslySetInnerHTML={{__html: capitalizeFirstLetter(post.body)}}></p>
             <LikeBlock postId={post.id}/>
         </main>
     );
